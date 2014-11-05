0b345b1456efb0a33708476191be49507084a5fb
Dan Morris <daniel.e.morris@gmail.com>
2014-11-05 09:47:16 -0800

    more automated continuous scraping, initial TFIDF and NMF functionality

--- nlp.py
+++ nlp.py
@@ -7,6 +7,9 @@ from nltk.util import ngrams
 from nltk.tokenize import word_tokenize
 from pymongo import MongoClient
 from collections import Counter
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
 def get_raw_docs(table):
     '''
@@ -31,6 +34,17 @@ def tokenize(doc):
             clean.append(word)
     return clean
 
+def clean_all_docs(table):
+    '''
+    Takes all documents in a table, cleans them with tokenize, and adds the
+        clean document back into their record as 'clean_text'
+    '''
+    for record in table.find({'full_text': {'$exists': True, '$ne': ''}}):
+        clean_doc = ' '.join(tokenize(record['full_text']))
+        table.update({'web_url': record['web_url']},
+                     {'$set': {'clean_text': clean_doc}},
+                     upsert=True)
+
 def simple_cluster(docs, nmax=1, topn=20):
     '''
     Returns the most common n-grams in the list of docs, from 1 to nmax
@@ -44,5 +58,44 @@ def simple_cluster(docs, nmax=1, topn=20):
             vocab[1][word] += 1
         for n in range(1, nmax):
             for g in ngrams(d, n+1):
-                vocab[n+1][g] += 1
+                vocab[n+1][' '.join(g)] += 1
     return {i:vocab[i].most_common(topn) for i in vocab}
+
+def table_tfidf(table, max_features=5000, ngram_range=(1, 1), max_df=.8):
+    vec = TfidfVectorizer(max_features=max_features,
+                            ngram_range=ngram_range,
+                            max_df=max_df)
+    X = vec.fit_transform([r['clean_text'] for r in table.find({'clean_text': \
+                                                        {'$exists': True}})])
+    return X, vec
+
+def basic_nmf(X, n_topics=20):
+    nmf = NMF(n_components=n_topics)
+    W = nmf.fit_transform(X)
+    H = nmf.components_
+    return W, H
+
+def topic_parse(X, vec, H, n_topics=20, n_top_words=20):
+    topics_dicts = []
+
+    for i in xrange(n_topics):
+        k, v = zip(*sorted(zip(vec.get_feature_names(), H[i]),
+                               key=lambda x: x[1])[:-n_top_words:-1])
+        val_arr = np.array(v)
+        norms = val_arr / np.sum(val_arr)
+        topics_dicts.append(dict(zip(k, np.rint(norms * 300))))
+    return topics_dicts
+
+def tfidf_nmf_analyze(table, max_features, ngram_range, max_df, n_topics,
+                      n_top_words):
+    '''
+    Combines the TFIDF -> NMF -> Topic Modeling pipeline into one function
+    '''
+    X, vec = table_tfidf(table, max_features, ngram_range, max_df)
+    W, H = basic_nmf(X, n_topics)
+    topic_dicts = topic_parse(X, vec, H, n_topics, n_top_words)
+
+    for i, topic in enumerate(topic_dicts):
+        print "Topic #" + str(i)
+        print sorted(topic.items(), key=lambda x: x[1])[::-1]
+        print '\n'
--- nyt_scrape.py
+++ nyt_scrape.py
@@ -11,6 +11,7 @@ notes: make sure mongod is up and running. use `sudo mongod` in terminal
 import requests
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+from time import sleep
 
 baseurl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'
 api_key = 'f5720d095a40f76fc4c1e2c4b4657e8f:15:57014703'
@@ -39,40 +40,77 @@ def single_query(url):
     docs = d['response']['docs'] # document records
     return docs
 
-def many_queries(url):
+def date_process(pub_date):
+    '''
+    Transforms "2002-12-31T00:00:00Z" into "20021231" so NYT likes it
+    '''
+    return ''.join(pub_date.split('T')[0].split('-'))
+
+def many_queries(q, param_dict):
+    '''
+    Designed to scrape as many results from the given query as possible.
+    Overcomes adversity and defeats the API limits!
+    '''
+    url = _query_url(q, param_dict)
     response = requests.get(url)
     d = response.json()
     hits = d['response']['meta']['hits'] # number of results
+    print 'Found ', hits, ' hits to the query'
     n = hits / 10
     docs = d['response']['docs']
+    present_date = date_process(docs[-1]['pub_date'])
     for p in range(1, n+1):
         # add the page number to the query, make it again
         new_url = url.split('api-key')
         new_url[0] += 'page=' + str(p) + '&'
         new_url = 'api-key'.join(new_url)
-        new_docs = single_query(new_url)
-        if new_docs != None:
-            docs += single_query(new_url)
-        else:
+        try: # don't lose the docs we already have if if borks
+            new_docs = single_query(new_url)
+            if new_docs != None:
+                docs += single_query(new_url)
+            else:
+                # reset the beginning of the date
+                present_date = date_process(docs[-1]['pub_date'])
+                print 'Last successful date: ', present_date
+                break
+        except:
+            print 'Broken.'
             break
-    return docs
+    return docs, present_date
 
 def load_mongo(table, docs):
     # load new documents into mongo table, checking for existence using web_url
     for d in docs:
+        d['full_text'] = ''
         table.update({'web_url': d['web_url']}, d, upsert=True)
 
 def get_full_text(web_url):
     '''
-    Scrapes article text from a single web_url, returns the string
+    Scrapes article text from a single web_url, returns the article as string.
     '''
     r = requests.get(web_url)
     if r.status_code != 200:
         print 'error: status code ', r.status_code
         return None
     s = BeautifulSoup(r.text, 'html.parser')
-    paragraphs = s.findAll('p', {'class':'story-body-text'})
-    story = ' '.join([p.text for p in paragraphs])
+
+    # New Style
+    if s.find('div', {'id':'mod-a-body-first-para'}) == None:
+        if s.find('p', {'class':'story-body-text'}) != None:
+            paragraphs = s.findAll('p', {'class':'story-body-text'})
+            story = ' '.join([p.text for p in paragraphs])
+        elif s.find('p', {'itemprop':'articleBody'}) != None:
+            paragraphs = s.findAll('p', {'itemprop':'articleBody'})
+            story = ' '.join([p.text for p in paragraphs])
+        else:
+            story = ''
+    # Old Style
+    else:
+        story = s.find('div', {'id':'mod-a-body-first-para'}).text
+        story += s.find('div', {'id':'mod-a-body-after-first-para'}).text
+
+    if story == '':
+        print 'bad scrape. web_url: ', web_url
     return story
 
 def load_full_texts(table, verbose=False):
@@ -81,12 +119,25 @@ def load_full_texts(table, verbose=False):
         document. If that field doesn't exist, we try to scrape it and add it
         in! Runs until some failure.
     '''
-    for record in table.find({'document_type':'article'}):
-        if record.get('full_text', None) == None:
-            story = get_full_text(record['web_url'])
+    for record in table.find({'document_type':'article', 'full_text':''}):
+        story = get_full_text(record['web_url'])
+        if verbose:
+            print story[:100]
+        table.update({'web_url': record['web_url']},
+                     {'$set': {'full_text': story}},
+                     upsert=True)
+
+def load_full_texts_from_docs(table, docs, verbose=False):
+    '''
+    Only looks at new documents to avoid going through the whole table each
+        scrape-iteration.
+    '''
+    for d in docs:
+        if table.find_one({'web_url':d['web_url']}) != None:
+            story = get_full_text(d['web_url'])
             if verbose:
-                print story[:50]
-            table.update({'web_url': record['web_url']},
+                print story[:100]
+            table.update({'web_url': d['web_url']},
                          {'$set': {'full_text': story}},
                          upsert=True)
 
@@ -95,7 +146,16 @@ def query_and_load_db(table, q, param_dict):
     Compresses the process into a single function!
     Just enter your table and your query info, sit back, and enjoy!
     '''
-    url = _query_url(q, param_dict)
-    docs = many_queries(url)
+    docs, present_date = many_queries(q, param_dict)
     load_mongo(table, docs)
-    load_full_texts(table)
+    load_full_texts(table, verbose=True)
+
+def troubleshoot_full_text(table):
+    '''
+    Returns a single example of a record whose full_text == '', so I can make
+        sure that the get_full_text module works for all sorts of documents.
+    '''
+    record = table.find_one({'full_text':''})
+    r = requests.get(record['web_url'])
+    s = BeautifulSoup(r.text, 'html.parser')
+    return s
