2076b5411b82ebd61619ee2937d5b9ddbcafc2b8
Dan Morris <daniel.e.morris@gmail.com>
2014-11-07 16:42:20 -0800

    oh yea, this is necessary

--- mongo_stuff.py
+++ mongo_stuff.py
@@ -13,3 +13,12 @@ def records_by_month(table, query={}):
         month = record['pub_date'][:7]
         mc[month] += 1
     return mc
+
+def just_clean_text(table, query={}):
+    '''
+    Queries the table and returns a list of clean text documents (id, text)
+    '''
+    query['clean_text'] = {'$exists': True, '$ne': ''}
+    cursor = table.find(query)
+    results = [(c['_id'], c['clean_text']) for c in cursor]
+    return results
